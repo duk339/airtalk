12ae58ea040863bdd7e15b3066b83d85691ae2f3
EC2 Default User <ec2-user@ip-172-31-38-242.ap-northeast-1.compute.internal>
2024-12-11 13:06:27 +0000

    Finish1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+      flash[:notice] = "更新に成功しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = '退会しました。'
+    redirect_to new_user_registration_path
   end
 
        private
@@ -18,5 +36,9 @@ class UsersController < ApplicationController
     unless user.id == current_user.id
       redirect_to user_path(current_user.id)
     end
+  end
+
+  def set_user
+    @user = User.find_by(:id => params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  resources :posts
 
   devise_for :users
-  resources :users, only: [:show, :edit]
+
+  resources :posts
+
+  resources :users, only: [:show, :edit, :update, :destroy]
   root to: "homes#top"
   get "/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
